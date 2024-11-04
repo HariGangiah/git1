1cd8d4f2ba72f93df84a6e4f3d94e7f614c0fb66
HariGangaiah <harigangaiah@gmail.com>
2024-11-04 17:19:08 +0530

    Update sysperf.sh

--- sysperf.sh
+++ sysperf.sh
@@ -10,3 +10,6 @@ free -m | awk 'NR==2 {printf "Used Memory: %s MB (%.2f%%)\n", $3, ($3/$2)*100}'
 echo ""
 echo "Disk Utilization"
 df -h | awk '{print$5}' | sed -n 2p
+
+
+echo "System Perforamnce Checked"
